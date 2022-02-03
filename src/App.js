a4acd4a53a1035145800dae859901fa850849789
Eimantas20 <mdwork20@gmail.com>
2022-02-03 20:14:20 +0200

    scroll bar, display multi items

--- src/App.js
+++ src/App.js
@@ -6,22 +6,42 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TestAbout from './components/TestAbout';
 import TestContacts from './components/TestContacts';
 import TestHome from './components/TestHome';
+import { useEffect, useState } from 'react';
 
 
 function App() {
-  return (
-    <Router>
-      <div className="app-container">
-        <Navigation />
-        <Conversation />
-          <Routes>
-            <Route  path='/about' element={<TestAbout />} />
-            <Route  path='/contacts' element={<TestContacts />} />
-            <Route exact path='/' element={<TestHome />} />
-          </Routes>
-      </div>
-    </Router>
-  );
+    const [ items, setItems ] = useState([]);
+
+    useEffect(()=> {
+        getAmountOfItems()
+        window.addEventListener('resize', getAmountOfItems);
+    }, [])
+
+
+    const getAmountOfItems = () => {
+        let width = window.innerWidth;
+        let arr = [...Array(Math.floor(width / 550))];
+        if (arr.length === 0) arr[0] = 0
+        setItems(arr);
+    }
+    
+    return (
+        <Router>
+            <div className="app-container">
+                <Navigation />
+                <div className='multi-conver-cont'>
+                    {items.map(item => (
+                        <Conversation />
+                    ))}
+                </div>
+                <Routes>
+                    <Route  path='/about' element={<TestAbout />} />
+                    <Route  path='/contacts' element={<TestContacts />} />
+                    <Route exact path='/' element={<TestHome />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
